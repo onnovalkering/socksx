7986377e62a06bb6fd306703a60a4a057300d37b
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-03-08 13:12:13 +0000

    chore: update benchmark scripts

--- benchmarks/entrypoint-httping.sh
+++ benchmarks/entrypoint-httping.sh
@@ -6,20 +6,18 @@ HTTPING_COUNT="${1}"
 HTTPING_INTERVAL="${2}"
 HTTPING_TARGET="${3}"
 PROXY_HOST="${4:-}"
-PROXY_VERSION="${5-5}"
+PROXY_PORT="${5-1080}"
+PROXY_VERSION="${6-5}"
 
 if [ ! -z "$PROXY_HOST" ]; then
     echo "Using proxy."
 
     iptables -t nat -A OUTPUT ! -d $PROXY_HOST/32 -o eth0 -p tcp -m tcp -j REDIRECT --to-ports 42000
-    ./redirector --socks $PROXY_VERSION $PROXY_HOST &
+    ./redirector $PROXY_HOST $PROXY_PORT --socks $PROXY_VERSION &
 
     sleep 1s
 else 
     echo "Not using proxy."
 fi
-
-# Wait until the next full minute.
-sleep $((60 - $(date +%S) ))
 
 httping -S --count $HTTPING_COUNT --interval $HTTPING_INTERVAL $HTTPING_TARGET
--- benchmarks/entrypoint-wrk.sh
+++ benchmarks/entrypoint-wrk.sh
@@ -7,20 +7,18 @@ WRK_DURATION="${2}"
 WRK_THREADS="${3}"
 WRK_TARGET="${4}"
 PROXY_HOST="${5:-}"
-PROXY_VERSION="${6-5}"
+PROXY_PORT="${6-1080}"
+PROXY_VERSION="${7-5}"
 
 if [ ! -z "$PROXY_HOST" ]; then
     echo "Using proxy."
 
     iptables -t nat -A OUTPUT ! -d $PROXY_HOST/32 -o eth0 -p tcp -m tcp -j REDIRECT --to-ports 42000
-    ./redirector --socks $PROXY_VERSION $PROXY_HOST &
+    ./redirector $PROXY_HOST $PROXY_PORT --socks $PROXY_VERSION &
 
     sleep 1s
 else 
     echo "Not using proxy."
 fi
 
-# Wait until the next full minute.
-sleep $((60 - $(date +%S) ))
-
-wrk --connections $WRK_CONNECTIONS --duration $WRK_DURATION --threads $WRK_THREADS $WRK_TARGET
+wrk --header "Connection: Close" --latency --connections $WRK_CONNECTIONS --duration $WRK_DURATION --threads $WRK_THREADS $WRK_TARGET
