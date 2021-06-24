3966d0e7f95a091e3b47c20ce63a0cf8bba0d75b
Onno Valkering <onnovalkering@users.noreply.github.com>
2021-06-24 09:21:08 +0000

    chore: add socksx-py crate

---
+++ socksx-py/src/lib.rs
@@ -0,0 +1,17 @@
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+use pyo3::wrap_pymodule;
+
+mod socks6;
+use socks6::*;
+
+#[pymodule]
+fn socksx(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pymodule!(socks6))?;
+
+    let sys = PyModule::import(py, "sys")?;
+    let sys_modules: &PyDict = sys.getattr("modules")?.downcast()?;
+    sys_modules.set_item("socksx.socks6", m.getattr("socks6")?)?;
+
+    Ok(())
+}
---
+++ socksx-py/src/socks6/mod.rs
@@ -0,0 +1,10 @@
+use pyo3::prelude::*;
+
+mod client;
+
+#[pymodule]
+fn socks6(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<client::Client>()?;
+
+    Ok(())
+}
